43af6bf7c694d306e0a81f956b61bdea5212b993
Philipp Miller <philipp.miller@tum.de>
2013-06-24 08:19:00 +0200

    Added auto scrolling to chat.js + refactoring js

--- js/chat.js
+++ js/chat.js
@@ -1,80 +1,54 @@
-var playerName = "Phil";
+var playerName = 'Phil';
 //var playerID = 1;
 
+// DOM vars
+var chatLog;
+var chatLogFrame;
+var chatText;
+var chatSubmit;
+
+// init
 $(function() {
+	chatLog      = $('#chatLog');
+	chatLogFrame = $('#chatLogFrame');
+	chatText     = $('#chatText');
+	chatSubmit   = $('#chatSubmit');
 	
-	var today = new Date();
+	chatSubmit.click(sendMessage);
 	
-	$("#chatSubmit").click(function() {
-		
-		var msg = $("#chatText").val();
-		
-		if (msg) {
-			
-			var msgTime = today.getHours()+":"+today.getMinutes();
-			
-			/*msgData = "msgTime="+msgTime
-					+ "&playerName="+playerName
-					+ "&msg="+msg;*/
-			msgData = "controller=Game"
-					+ "&gameId=" + "1" // TODO
-					+ "&method=post"
-					+ "&msg=" + msg;
-			
-			$.ajax({  
-				type: "POST",  
-				//url: "lib/ajaxAnswer.php",  
-				url: "index.php/ajax",
-				data: msgData,  
-				success: function(msgTpl) {  
-					$("#chatLog").append(msgTpl);
-					/*msg += ajaxAnswer;
-					var msgHTML = "<p class=\"msgOwn\">"
-								+ "	<span class=\"msgTime\">"+msgTime+"</span>"
-								+ "	<span class=\"msgAuthor\">"+playerName+"</span>"
-								+ "	<span class=\"msgText\">"+msg+"</span>"
-								+ "</p>";
-					$("#chatLog").append(msgHTML);*/
-					$("#chatText").val("");
-					scrollChatToBottom();
-		    	}
-		    });
-		}
-		return false;
-	});
+	scrollChatToBottom();
 });
 
-$(function() {
-	$( ".chesspiece" ).draggable({
-		containment: "table#chessboardTable"
-		, cursor: 'move'
-		//, snap: "table#chessboardTable td.square"
-		, stack: '.chesspiece'
-		//, revert: true
-	});
-	$( "table#chessboardTable td.square" ).droppable({
-		accept: ".chesspiece",
-		drop: function( event, ui ) {
-			//$("#chatText").val("asdf");
-			var chesspiece = ui.draggable;
-			
-			//chesspiece.draggable( 'disable' );
-			//$(this).droppable( 'disable' );
-			//chesspiece.position(" { of: $(this), my: 'left top', at: 'left top' } ");
-			chesspiece.draggable( 'option', 'revert', false );
+// functions
 
-			alert( 'The chesspiece #'
-				+ chesspiece.attr('id')
-				+ ' was dropped onto the square #'
-				+ $(this).attr('id') );
-		}
-	});
-});
+function sendMessage() {
+	var msg = chatText.val();
+	if (msg) {
+		msgData = 'controller=Game'
+				+ '&gameId=' + '1' // TODO
+				+ '&method=post'
+				+ '&msg=' + msg;
+		
+		$.ajax({  
+			type: 'POST',  
+			url: 'index.php/ajax',
+			data: msgData,  
+			success: showMessage
+	    });
+	}
+	return false;
+}
 
-//TODO
+function showMessage(msgTpl) {  
+	chatLog.append(msgTpl);
+	chatText.val('')
+	              .focus();
+	scrollChatToBottom();
+}
+
 function scrollChatToBottom() {
-	$('#chatLog').animate({ 
-		scrollTop: $('#chatLog > div').height()-$('#chatLog').height()}, 
-		200, "swing"
+	chatLogFrame.animate(
+		{scrollTop: chatLog.height() - chatLogFrame.height()}, 
+		200
 	);
 }
