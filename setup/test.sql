50458c523f0f7b01cd8f770920784a4de22868ee
Philipp Miller <philipp.miller@tum.de>
2013-06-26 17:59:25 +0200

    Added setup script with minimal database and testing data. Read the introduction in setup.php for usage information!

---
+++ setup/test.sql
@@ -0,0 +1,5 @@
+INSERT INTO `user` (userName, email, password, cookieHash, language) VALUES
+	('phil', 'phil@mill.er', 'test', 'cookiesareyummy', ''),
+	('larissa', 'larissa@hamm.er', 'test', 'cookiesareyummy', 'de'),
+	('bare', 'bare@example.com', 'test', 'cookiesareyummy', 'none'),
+	('someguywithareallylongname', 'someguywithareallylongname@reallylongdomain.topleveldomain', 'test', 'cookiesareyummy', 'en');
